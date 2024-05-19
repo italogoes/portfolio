bee80ff158517e22519ba8d676d8fedc833d544d
Italo Goes <italogoes85@gmail.com>
2024-05-19 18:02:59 -0300

    sessao numbers

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,46 @@
 window.addEventListener("DOMContentLoaded", () => {
-    const element = document.getElementById('count-one')
+    const elementOne = document.getElementById('count-one')
+    const elementTwo = document.getElementById('count-two')
+    const elementThree = document.getElementById('count-three')
+    const elementFour = document.getElementById('count-four')
     
-    let count = 0;
+    let countOne = 0;
     let interval = setInterval(() => {
-        if (count < 150) {
-            count++;
-            element.innerHTML = count
+        if (countOne < 150) {
+            countOne++;
+            elementOne.innerHTML = countOne
         } else {
             clearInterval(interval);
+        }
+    }, 10);
+
+    let countTwo = 0
+    let intervalTwo = setInterval(() => {
+        if (countTwo < 10) {
+            countTwo++
+            elementTwo.innerHTML = countTwo
+        } else {
+            clearInterval(intervalTwo)
+        }
+    }, 10);
+
+    let countThree = 0
+    let intervalThree = setInterval(() => {
+        if (countThree < 50) {
+            countThree++
+            elementThree.innerHTML = countThree
+        } else {
+            clearInterval(intervalThree)
+        }
+    }, 10);
+
+    let countFour = 0
+    let intervalFour = setInterval(() => {
+        if (countFour < 100) {
+            countFour++
+            elementFour.innerHTML = countFour
+        } else {
+            clearInterval(intervalFour)
         }
     }, 10);
 })
